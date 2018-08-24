3602a5464c62a7173cf1b45b4f750b93ed4d7ac7
Nate <nathanestes@gmail.com>
2018-08-24 14:40:12 -0600

    initial and last

---
+++ bt.rb
@@ -0,0 +1,32 @@
+
+
+
+def intro
+puts "Enter some text"
+gets
+end
+
+@input.gets.strip.to_s
+
+def checkp
+  alph = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N",
+          "O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  
+  #array map - check each index for a-z value
+
+end
+
+# def input
+
+#   if @input index0 = indexlast
+#     puts True
+
+#   else puts False
+#   end
+
+while True
+  intro
+end
+
+
+
